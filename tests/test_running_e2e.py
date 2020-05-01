304a7ae363dd8ffa942621b847546cfba3d5ac4a
Alexandre Cameron <lexcam@hotmail.fr>
2020-05-01 22:46:02 +0200

    test: using inputs within code

--- tests/test_running_e2e.py
+++ tests/test_running_e2e.py
@@ -14,7 +14,7 @@ from boar.__init__ import Notebook
      {'outputs_1': {'b': 'B'}, 'outputs_3': {'d': 2}, 'outputs_5': {'f': 78.56}}
      ),
 ])
-def test_run_notebook_exports_params(
+def test_run_notebook_exports_outputs(
     notebook_path: Union[str, Path],
     expected_outputs: dict,
 ) -> None:
@@ -25,6 +25,30 @@ def test_run_notebook_exports_params(
     # When
     outputs = run_notebook(
         notebook_path=notebook_path,
+        verbose=verbose,
+    )
+
+    # Then
+    assert outputs == expected_outputs
+
+
+@pytest.mark.e2e
+@pytest.mark.parametrize("new_value", [
+    "imported value", "not B", 12, 34.56,
+])
+def test_run_notebook_skips_line_and_used_inputs(
+    new_value: Union[str, float],
+) -> None:
+    # Given
+    from boar.running import run_notebook
+    notebook_path = Path(Notebook._01.value, "simple_inputs.ipynb")
+    expected_outputs = {'outputs': {'a': 0, 'b': new_value, 'c': 57}}
+    verbose = True
+
+    # When
+    outputs = run_notebook(
+        notebook_path=notebook_path,
+        inputs={"BBB": new_value},
         verbose=verbose,
     )
 
