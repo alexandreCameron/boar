082300d39d076f390943499018273d47343ef9e7
Alexandre Cameron <lexcam@hotmail.fr>
2020-05-09 22:58:00 +0200

    style: set back version to patch

--- version.py
+++ version.py
@@ -2,4 +2,4 @@ import datetime
 
 VERSION = "0.0.1"
 PATCH = datetime.datetime.utcnow().strftime("%y%m%d%H%M")
-RELEASE = f"{VERSION}"  # f"{VERSION}-{PATCH}"
+RELEASE = f"{VERSION}-{PATCH}"  # f"{VERSION}"
