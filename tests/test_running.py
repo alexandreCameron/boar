771537aa03b50e5da24096627e1a9e39b20c81a8
Alexandre Cameron <lexcam@hotmail.fr>
2020-05-09 21:59:42 +0200

    feat: add check is notebooki in testing

--- tests/test_running.py
+++ tests/test_running.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 import pytest
 from unittest.mock import patch, call, Mock
 
@@ -6,22 +5,28 @@ from unittest.mock import patch, call, Mock
 @pytest.mark.ut
 @patch("boar.running.close_plots")
 @patch("boar.running.get_code_sources")
+@patch("boar.running.check_is_notebook")
 def test_run_notebook_calls_functions_in_order(
+    mock_check_is_notebook,
     mock_get_code_sources,
     mock_close_plots,
 ) -> None:
     # Given
     from boar.running import run_notebook
-    notebook_path = Path("my_notebook.ipynb")
+    from boar.__init__ import Notebook
+    notebook_path = next(Notebook._00.value.iterdir())
     sources = []
     expected_outputs = {}
 
     # Thus
     mock_manager = Mock()
+    mock_manager.attach_mock(mock_check_is_notebook, "mock_check_is_notebook")
     mock_manager.attach_mock(mock_get_code_sources, "mock_get_code_sources")
     mock_manager.attach_mock(mock_close_plots, "mock_close_plots")
+    mock_check_is_notebook.return_value = notebook_path
     mock_get_code_sources.return_value = sources
     expected_function_calls = [
+        call.mock_check_is_notebook(notebook_path),
         call.mock_get_code_sources(notebook_path),
         call.mock_close_plots(),
     ]
